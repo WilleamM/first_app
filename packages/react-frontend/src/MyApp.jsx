c43e12b3413bc720211bf2fc4afc2c5134ede902
WilleamM <mendezwilleam@icloud.com>
2025-09-26 13:18:43 -0700

    Added form functionality, submitting next

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form"
 
 // <Table characterData={characters}/> where characters is being passed to table as a prop
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index){
         const updated = characters.filter((character, i) => {
@@ -35,6 +19,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
         />
+        <Form />
     </div>
     );
 }
