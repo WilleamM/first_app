6ab031492c3a144660ccb0f82bcdf59e814db5a5
WilleamM <mendezwilleam@icloud.com>
2025-10-21 14:19:05 -0700

    Added Mongo DBCloud connection string

--- packages/express-backend/user-services.js
+++ packages/express-backend/user-services.js
@@ -4,8 +4,9 @@ import userModel from "./user.js";
 mongoose.set("debug", true);
 
 mongoose
-  .connect("mongodb://localhost:27017/users")
-  .catch((error) => console.log(error));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas!"))
+  .catch((error) => console.log("Error connection to MongoDB Atlas: ",error));
 
 function getUsers(name, job) {
   let promise;
